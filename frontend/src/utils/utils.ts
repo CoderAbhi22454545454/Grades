6014e6a5f989a9b462cabb24c751aaa30336c090
Abhishek <abhishekmohalkar007@gmail.com>
2024-09-01 11:46:42 +0530

    first commit

---
+++ frontend/src/utils/utils.ts
@@ -0,0 +1,8 @@
+const serverURL = "http://localhost:8080"
+const hostname = "https://evaluateai.vercel.app";
+const currencySymbol = "₹";
+const appName = "GRADES";
+const maxFileCount = 10;
+const maxFileSize = "8MB"; // 8MB
+
+export { serverURL, currencySymbol, appName, maxFileCount, maxFileSize, hostname };
